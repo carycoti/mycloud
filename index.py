5c676251bab9a273961ee200997769e2561b23d9
Kung Liao <kangla-122@163.com>
2020-10-30 17:49:11 +0800

    some

--- index.py
+++ index.py
@@ -2,26 +2,38 @@
 
 import os
 import io
+
 title='mycloud'
 url = f'https://cdn.jsdelivr.net/gh/carycoti/{title}/'
 gitignore = ['.gitignore', '.git']
 with open('.gitignore', 'r') as f:
         for item in f:
             gitignore.append(item)
-def make(s):
-    d=os.listdir('.')
-    with open('index.html','w') as f:
-        f.write(f'<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>{title}</title></head><body><h1>{title}</h1><ul>\n')
-        f.write('<h2>Sitemap</h1><ul>\n')
-        for i in d:
-            if(not [False for item in gitignore if i in item]):
-                if(os.path.isdir(i)):
-                    os.chdir(i)
-                    make(s+i+'/')
-                    os.chdir('..')
-                    f.write(f'<li><a href="{url}{i}">{i}/</a></li>\n')
-                else:
-                    f.write(f'<li><a href="{url}{i}">{i}</a></li>\n')
 
-make('https://github.com/carycoti/mycloud/')
+
+def make(s, f):
+    d=os.listdir('.')
+    f.write('<ul>\n')
+    for i in d:
+        if(not [False for item in gitignore if i in item]):
+            if os.path.isdir(i):
+                f.write(f'<li><a href="{s}{i}/">{i}/</a>')
+                os.chdir(i)
+                make(s+i+'/', f)
+                os.chdir('..')
+                f.write('</li>\n')
+            else:
+                f.write(f'<li><a href="{s}{i}">{i}</a></li>\n')
+    f.write('</ul>\n')
+
+
+with open('index.html','w') as f:
+    f.write(f'<!DOCTYPE html>\n<html>\n')
+    f.write(f'<head>\n<title>{title}</title>\n')
+    f.write(f'<meta http-equiv="Content-Typecontent="text/html; charset=UTF-8" />\n')
+    f.write(f'</head>\n<body>\n<h1>{title}</h1>\n')
+    f.write('<h2>Sitemap</h2>\n')
+    make(url, f)
+    f.write('</body>\n</html>')
+    
 print('DONE')
